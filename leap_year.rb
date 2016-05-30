6e48880b56a73b0a59d72b81a10e33337967a9f1
hasna <hasnadhbdl@gmail.com>
2016-05-30 22:20:21 +0100

    ruby codes to calculate prime number, fibonacci, celcius to fahrenheit

---
+++ leap_year.rb
@@ -0,0 +1,14 @@
+puts "Enter a year"
+		a=gets.chomp
+			if(a%4==0 && a%100!=0)
+				{
+					if(a%400==0)
+
+				}
+
+			}
+			puts "is a leap year"
+		else
+			{
+				puts "not a leap year"
+			}
---
+++ prime_number.rb
@@ -0,0 +1,8 @@
+def is_prime n
+  for i in 2..(n-1)
+   if (n % 2) == 0
+    return false
+   end
+  end
+  	true
+ end
